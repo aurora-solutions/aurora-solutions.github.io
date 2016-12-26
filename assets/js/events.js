7542ed3ba903056a7d7c6a638811e54056a061cc
Zain Zafar <zain@aurorasolutions.io>
2016-12-26 15:55:35 +0500

    Add directive to handle mouseover events for tooltip

--- assets/js/events.js
+++ assets/js/events.js
@@ -11,21 +11,4 @@
         }, 1250, 'easeInOutExpo');
     });
 
-    $(window).load(function() {
-        var tooltipOtherFrameworks = $('.tooltip-other');
-        var tooltipMultiText = $('.tooltip-multi-text');
-
-        tooltipOtherFrameworks.mouseover(function(event) {
-
-            var left = ($(this).offset().left - 20) + 'px';
-            var top = ($(this).offset().top - tooltipMultiText.height() - 10) + 'px';
-            tooltipMultiText.css({ visibility: 'visible', top: top, left: left });
-
-
-        });
-        tooltipOtherFrameworks.mouseout(function() {
-            tooltipMultiText.css({ visibility: 'hidden' });
-        });
-    });
-
 })(jQuery); // End of use strict
